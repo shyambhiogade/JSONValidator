400dfa2b34537b1d15980cfaed782f4c7334d10d
shyambhiogade <shyam.bhiogade@gmail.com>
2017-08-21 08:22:42 -0700

    Add files via upload

--- src.js
+++ src.js
@@ -1,13 +1,69 @@
 $(function () {
 
-    var inputJSONObj = { "glossary": { "title": "example glossary", "GlossDiv": { "title": "S", "GlossList": { "GlossEntry": { "ID": "SGML", "SortAs": "SGML", "GlossTerm": "Standard Generalized Markup Language", "Acronym": "SGML", "Abbrev": "ISO 8879:1986", "GlossDef": { "para": "A meta-markup language, used to create markup languages such as DocBook.", "GlossSeeAlso": ["GML", "XML"] }, "GlossSee": "markup" } } } } }
-    $("#jsonTextArea").val(JSON.stringify(inputJSONObj));
+    // set the size 
+    var setSizeForControl = function () {
+        $("#jsonContainer").height((window.innerHeight - $("#header").outerHeight() - $("#actionContainer").outerHeight()) + "px");
+        $("#jsonContainer").width(window.innerWidth + "px");
+        // set the layoutout 
+        var jsonContainerWidth = $("#jsonContainer").width();
+        $("#jsonContainer").width(jsonContainerWidth);
+        var halfWidth = (jsonContainerWidth) / 2;
+        $("#treePanel").css("width", halfWidth + "px");
+        $("#jsonTextArea").css("width", halfWidth + "px");
 
-    // set the layoutout 
-    var jsonContainerWidth = $("#jsonContainer").width();
-    var halfWidth = jsonContainerWidth / 2;
-    $("#treePanel").css("width", halfWidth + "px");
-    $("#jsonTextArea").css("width", halfWidth + "px");
+        var splitter = $("#splitter");
+        splitter.css("left", halfWidth + "px");
+        splitter.height($("#treePanel").height());
+    }
+
+    var cookieName = "jsonviewerCookie";
+    // Cookies
+    function createCookie(name, value, days) {
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            var expires = "; expires=" + date.toGMTString();
+        }
+        else var expires = "";
+
+        document.cookie = name + "=" + value + expires + "; path=/";
+    }
+
+    // read cookies
+    function readCookie(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+        }
+        return null;
+    }
+
+    setSizeForControl();
+
+    $(window).resize(function () {
+        setTimeout(function () { setSizeForControl(); }, 100);
+    });
+
+    var inputJSONObj = {
+        "name": "John",
+        "married":true,
+        "age": 30,
+        "cars": [
+            { "name": "Ford", "models": ["Fiesta", "Focus", "Mustang"] },
+            { "name": "BMW", "models": ["320", "X3", "X5"] },
+            { "name": "Fiat", "models": ["500", "Panda"] }
+        ]
+    };
+
+    var cookieData = readCookie(cookieName);
+    if (cookieData) {
+        $("#jsonTextArea").val(cookieData);
+    } else {
+        $("#jsonTextArea").val(JSON.stringify(inputJSONObj));
+    }    
 
     var formatJSONData = function () {
         var inputjsonData = $("#jsonTextArea").val();
@@ -20,7 +76,7 @@ $(function () {
     var toggleTree = function (eventObject) {
         console.log(eventObject);
         var toggleEle = $(eventObject.target)
-        var childList = toggleEle.parent().find("ul").first();
+        var childList = toggleEle.closest("li").find("ul").first();
         var childListDisplayValue = childList.css("display");
         if (childListDisplayValue === "none") {
             toggleEle.removeClass("expand");
@@ -46,8 +102,11 @@ $(function () {
         $.each(jsonObj, function (key, value) {
             var typeofValue = typeof (value)
             if (typeofValue === "object") {
-                var spanEle = $("<span class='toggleTree collapse inlineSpan'></span><span class='objectSpan'></span>");
-                spanEle.on("click", toggleTree);
+                var objHtml = "<span class='toggleTree collapse inlineSpan'></span><span class='objectSpan'></span>";
+                if (value.length) {
+                    objHtml = "<span class='toggleTree collapse inlineSpan'></span><span class='arraySpan'></span>";
+                }
+                var spanEle = $(objHtml);                
                 var parentUl = parentNode.find("ul").first();
 
                 if (value === null) {
@@ -56,9 +115,11 @@ $(function () {
                     parentUl.append(listItem);
                     listItem.prepend(spanEle);
                 } else {
-                    var listItem = $("<li>" + key + "</li>");
+
+                    var listItem = $("<li></li>");
+                    var divItem = $("<div class='eachItem'>" + key + "</div>").prepend(spanEle);
                     parentUl.append(listItem);
-                    listItem.prepend(spanEle);
+                    listItem.append(divItem);
                     addTree(value, listItem);
                 }
             } else {
@@ -70,14 +131,26 @@ $(function () {
                 } else if (typeofValue === "boolean") {
                     typeSpecificClass = "booleanSpan";
                 }
-                parentNode.find("ul").append("<li><span class='inlineSpan " + typeSpecificClass + "'></span>" + key + " : " + value + "</li>");
+                parentNode.find("ul").first().append("<li><div class='eachItem'><span class='inlineSpan " + typeSpecificClass + "'></span>" + key + " : " + value + "</div></li>");
             }
+        });
+        
+        // add toggle handler
+        $(".toggleTree").off("click");
+        $(".toggleTree").on("click", toggleTree);
+
+        // add click selection
+        $(".eachItem").off("click");
+        $(".eachItem").on("click", function (event) {
+            $(".eachItem").css("background-color", '');
+            $(event.currentTarget).css("background-color", "#cce6ff");
         });
     }
 
+
     var treeContainer = $("#treecontainer");
     var topJSONObj = { JSON: inputJSONObj };
-    addTree(topJSONObj, treeContainer);
+    addTree(topJSONObj, treeContainer);  
 
     $("#formatBtn").click(formatJSONData);
 
@@ -86,7 +159,7 @@ $(function () {
         var outputJSONData = JSON.stringify(JSON.parse(inputjsonData));
         $("#jsonTextArea").val(outputJSONData);
     });
-
+       
     $("#expandAllBtn").on("click", function () {
         $("#treecontainer").find("#topNode").find("ul").css("display", "");
         $("#treecontainer").find(".toggleTree").removeClass("expand").addClass("collapse");
@@ -96,7 +169,7 @@ $(function () {
         $("#treecontainer").find("#topNode").find("ul").css("display", "none");
         $("#treecontainer").find(".toggleTree").removeClass("collapse").addClass("expand");
     });
-
+    
     $("#treeViewBtn").on("click", function () {
         $("#topNode").remove();
         firstTime = true;
@@ -106,16 +179,9 @@ $(function () {
         addTree(topJSONObj, treeContainer);
     });
 
-    $("#showTreeViewBtn").on("click", function () {
-        $("#treeView").css("display", "block");
-        $("#textView").css("display", "none");
+    $('#jsonTextArea').bind('input propertychange', function () {
+        createCookie(cookieName, $('#jsonTextArea').val(), 360);
     });
-
-    $("#showTextViewBtn").on("click", function () {
-        $("#treeView").css("display", "none");
-        $("#textView").css("display", "block");
-    });
-
 
     // add splitter - container.
     var splitter = $("#splitter");
@@ -127,22 +193,27 @@ $(function () {
     var jsonContainer = $("#jsonContainer");
 
     var isDragging = false;
-    var downScreenX = 0;
+    var downpageX = 0;
     splitter.mousedown(function (event) {
-        downScreenX = event.screenX;
+        downpageX = event.pageX;
         isDragging = true;
     });
 
     jsonContainer.mousemove(function (event) {
-        if (isDragging && event.screenX > 100 & ((jsonContainer.width() - event.screenX) > 100)) {
+        // min
+        var minWidthAllowedForInnerContainer = 100;
+        var innerContainerMargin = 5;
+        if (isDragging && event.pageX > minWidthAllowedForInnerContainer &&
+            ((jsonContainer.width() - event.pageX) > minWidthAllowedForInnerContainer)) {
             jsonTextArea.css("cursor", "col-resize");
             treePanel.css("cursor", "col-resize");
-            var distance = event.screenX - downScreenX;
-            var newLeft = (splitter.position().left + distance);
-            splitter.css("left", event.screenX + 2 + "px");
-            var treePanelWidth = jsonContainer.width() - event.screenX - 5;
-            jsonTextArea.css("width", event.screenX - 5 + "px");
-            treePanel.css("width", treePanelWidth + "px");
+            var distance = event.pageX - downpageX;            
+            if (distance !== 0) {
+                splitter.css("left", event.pageX + 2 + "px");
+                var treePanelWidth = jsonContainer.width() - event.pageX - innerContainerMargin;
+                jsonTextArea.css("width", event.pageX - innerContainerMargin + "px");
+                treePanel.css("width", treePanelWidth + "px");
+            }
         } else {
             jsonTextArea.css("cursor", "default");
             treePanel.css("cursor", "default");
